be67a9bcf4d9ab3089669f7a7e6a3aba234d9675
Vedant Pimple <60323634+vedantsp@users.noreply.github.com>
2023-05-21 21:19:49 +0530

    Update recursion-example-2.c

--- Recursion/recursion-example-2.c
+++ Recursion/recursion-example-2.c
@@ -1,1 +1,25 @@
 
+#include <stdio.h>
+
+
+void func(int n)
+{
+  
+    if(n>0)
+    {
+      
+     func(n-1);  
+     printf("%d",n);    
+    
+      
+    }
+    
+}
+
+void main()
+{
+  
+    int x=3;
+    func(x);
+  
+}
