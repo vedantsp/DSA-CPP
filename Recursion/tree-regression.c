dc6526779aaaf7f5b309b9664f2e065895c5f72d
Vedant Pimple <60323634+vedantsp@users.noreply.github.com>
2023-05-23 16:18:03 +0530

    Create tree-regression.c

---
+++ Recursion/tree-regression.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void func(int n)
+{
+    if(n>0)
+    {
+    printf("%d",n);    
+    func(n-1);
+    func(n-1);
+    }
+    
+}
+
+void main()
+{
+    int x=3;
+    func(x);
+}
