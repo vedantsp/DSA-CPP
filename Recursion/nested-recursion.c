2ab33fdefdec39a09144ff56feba837b3bbfb926
Vedant Pimple <60323634+vedantsp@users.noreply.github.com>
2023-05-23 17:43:24 +0530

    Update nested-recursion.c

--- Recursion/nested-recursion.c
+++ Recursion/nested-recursion.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+
+
+
+int funA(int n){
+    
+    if(n>100)
+    
+        return n-10;
+    
+        return funA(funA(n+11));
+}
+
+
+int main()
+{
+    int r;
+    r=funA(30);
+    printf("%d",r);
+    return 0;
+}
