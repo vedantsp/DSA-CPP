01fcc7c0d861da5737fcfcb31d80fe802b47ce5a
Vedant Pimple <60323634+vedantsp@users.noreply.github.com>
2023-05-29 18:41:04 +0530

    Create power-better-optimization.cpp

---
+++ Recursion-Applications/power-better-optimization.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int pow(int m, int n)
+{
+    if(n==0)
+    return 1;
+    
+    if(n%2==0)
+    return pow(m*m,n/2);
+    
+    else
+    return m*pow(m*m,(n-1)/2);
+}
+
+int main()
+{
+    int r,f,d;
+    d=pow(3,3);
+    cout<<d;
+}
