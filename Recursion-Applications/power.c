05d407abc09d9a2a76268bcd65b84a02b97fa287
Vedant Pimple <60323634+vedantsp@users.noreply.github.com>
2023-05-29 18:31:02 +0530

    Update power.c

--- Recursion-Applications/power.c
+++ Recursion-Applications/power.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+int pow(int m, int n)
+{
+    if(n==0)
+    return 1;
+    else
+    return pow(m,n-1)*m;
+}
+
+int main()
+{
+    int r,f,d;
+    d=pow(3,2);
+    printf("%d",d);
+}
