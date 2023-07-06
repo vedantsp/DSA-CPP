b7ad19eb5a817c1287b281c0824431fda1861008
Vedant Pimple <60323634+vedantsp@users.noreply.github.com>
2023-07-06 18:07:06 +0530

    Update Second largest in the array brute.cpp

--- Arrays  Problems/Second largest in the array brute.cpp
+++ Arrays  Problems/Second largest in the array brute.cpp
@@ -1,1 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+void getElements(int arr[], int n)
+{
+    if(n==0 || n==1)
+        cout<<-1<<" "<<-1<<endl;
+    sort(arr,arr+n);
+    int secondSmall = arr[1];
+    int secondLargest = arr[n-2];
+    cout<<"Second smallest is : "<<secondSmall<<endl;
+    cout<<"Second Largest is :  "<<secondLargest;
+}
+
+int main()
+{
+    int arr[] = {1,2,5,3,7,8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    getElements(arr,n);
+    return 0;
+}
